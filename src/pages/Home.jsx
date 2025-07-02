fa503d6b9f8383b15e3f92665cc72715d6aa2084
Your Name <you@example.com>
2025-07-02 12:56:02 +0530

    added import config using vite.config and jsconfig

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Footer from "components/Footer/Footer";
+
+function Home(){
+
+    return(
+        <>
+          Home Component
+          <Footer />
+        </>
+    )
+
+}
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { Component } from 'react';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  
+  resolve: {
+    alias:{
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+      layout: "/src/layout",
+      Redux: "/src/Redux",
+      config: "/src/config",
+      helpers: "/src/helpers",
+      routes: "/src/routes",
+    }
+  }  
 })
